41b1c5b4050b7218be2bbf95663a56830817969b
Vitaliy Filippov <vitalif@mail.ru>
2009-04-01 11:01:35 +0000

    Bug 47974 Now templates {{WikiCutBegin|<TITLE>}} ... {{WikiCutEnd}} Remove ShowHide extension

--- custisinstall/ServerSettings.php
+++ custisinstall/ServerSettings.php
@@ -1,9 +1,10 @@
 <?php
+
 require_once('BaseSettings.php');
 require_once($IP.'/extensions/WhoIsWatching/SpecialWhoIsWatching.php');
+
 $wgPageShowWatchingUsers = true;
 
-require_once($IP.'/extensions/ShowHide/ShowHide.php');
 require_once($IP.'/extensions/AnyWikiDraw/AnyWikiDraw.php');
 require_once($IP.'/extensions/Polls/poll.php');
 require_once($IP.'/extensions/mediawikiquizzer/mediawikiquizzer.php');
@@ -16,21 +17,23 @@ $wgEnotifWatchlist     = true; // UPO
 $wgEmailAuthentication = true;
 $wgEnotifMinorEdits    = true;
 $wgCookieHttpOnly      = false;
+$wgEmailContentType    = 'text/plain';
 
-$wgEmergencyContact = "stas@custis.ru";
-$wgPasswordSender   = "stas@custis.ru";
-
-
+$wgEmergencyContact    = "stas@custis.ru";
+$wgPasswordSender      = "stas@custis.ru";
 
 $wgAllowExternalImages     = true;
-$wgAllowExternalImagesFrom = array( 'http://penguin.office.custis.ru/',
-                                    'http://svn.office.custis.ru/',
-                                    'http://plantime.office.custis.ru/');
+$wgAllowExternalImagesFrom = array(
+    'http://penguin.office.custis.ru/',
+    'http://svn.office.custis.ru/',
+    'http://plantime.office.custis.ru/'
+);
 
-
-$wgSMTP = array("host" => 'localhost',
-           "IDHost" => 'custis.ru',
-           "port" => "25",
-           "auth" => false);
+$wgSMTP = array(
+    "host"   => 'localhost',
+    "IDHost" => 'custis.ru',
+    "port"   => "25",
+    "auth"   => false,
+);
 
 ?>
