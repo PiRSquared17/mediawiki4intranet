a91221586c4e9c2305091934894653ed17c0b1db
Vitaliy Filippov <vitalif@mail.ru>
2009-07-16 14:32:29 +0000

    Bug 51808 Version

--- tools/external-edit-xpi-helper/do.bat
+++ tools/external-edit-xpi-helper/do.bat
@@ -1,2 +1,4 @@
-jar cvf chrome/wikihelper.jar content locale
-jar cvf WikiHelper.xpi COPYING chrome.manifest install.rdf chrome
+rm chrome/wikihelper.jar
+rm WikiHelper.xpi
+zip -r chrome/wikihelper.jar content locale -x '*/.svn/*'
+zip -r WikiHelper.xpi COPYING chrome.manifest install.rdf chrome -x '*/.svn/*'
