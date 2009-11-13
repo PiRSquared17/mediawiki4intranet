922ba848b597faf35d8e250618b76b778abf8eb1
Vitaliy Filippov <vitalif@mail.ru>
2009-11-13 18:43:54 +0000

    Bug 56578 I FOUND IT!!! :)))))) Why SphinxSearch was working so bad? ? ?????? ??? ? ???? ?????????? ?? ????. ? ?????? ?? ????? ??????? ??????. Titles are stored in the DB with spaces replaced to _

--- sphinxsearch/sphinxconf.py
+++ sphinxsearch/sphinxconf.py
@@ -1,4 +1,5 @@
 # -*- coding: utf-8 -*-
+
 import os
 import os.path
 import glob
@@ -12,161 +13,156 @@ import re
 reload(sys)
 sys.setdefaultencoding('utf-8')
 
-wikis=[]
-
-
+wikis = []
 
 def print_wiki_conf(wiki):
-  ls="""
+  ls = """
 source src_main_%(wikiname)s
 {
-	type		= mysql
-	sql_host	= localhost
-	sql_user	= %(sql_user)s
-	sql_pass	= %(sql_pass)s
-	sql_db		= %(sql_db)s
-	sql_query_pre	= SET NAMES utf8
-	sql_query	= SELECT page_id, page_title, page_namespace, old_id, old_text FROM page, revision, text WHERE rev_id=page_latest AND old_id=rev_text_id 
-	sql_attr_uint	= page_namespace
-	sql_attr_uint	= old_id
-	sql_attr_multi  = uint category from query; SELECT cl_from, page_id AS category FROM categorylinks, page WHERE page_title=cl_to AND page_namespace=14
-	sql_query_info	= SELECT page_title, page_namespace FROM page WHERE page_id=$id
+    type           = mysql
+    sql_host       = localhost
+    sql_user       = %(sql_user)s
+    sql_pass       = %(sql_pass)s
+    sql_db         = %(sql_db)s
+    sql_query_pre  = SET NAMES utf8
+    sql_query      = SELECT page_id, REPLACE(page_title,'_',' ') page_title, page_namespace, old_id, old_text FROM page, revision, text WHERE rev_id=page_latest AND old_id=rev_text_id 
+    sql_attr_uint  = page_namespace
+    sql_attr_uint  = old_id
+    sql_attr_multi = uint category from query; SELECT cl_from, page_id AS category FROM categorylinks, page WHERE page_title=cl_to AND page_namespace=14
+    sql_query_info = SELECT REPLACE(page_title,'_',' ') page_title, page_namespace FROM page WHERE page_id=$id
 }
 
 source src_incremental_%(wikiname)s : src_main_%(wikiname)s
 {
-	sql_query	= SELECT page_id, page_title, page_namespace, old_id, old_text FROM page, revision, text WHERE rev_id=page_latest AND old_id=rev_text_id AND page_touched>=DATE_FORMAT(CURDATE(), '%%Y%%m%%d050000') 	
+    sql_query = SELECT page_id, REPLACE(page_title,'_',' ') page_title, page_namespace, old_id, old_text FROM page, revision, text WHERE rev_id=page_latest AND old_id=rev_text_id AND page_touched>=DATE_FORMAT(CURDATE(), '%%Y%%m%%d050000')     
 }
 
 index main_%(wikiname)s
 {
-	source		= src_main_%(wikiname)s
-	path		  = /var/data/sphinx/main_%(wikiname)s
-	docinfo		= extern
-	morphology	= stem_ru
-	#stopwords	= /var/data/sphinx/stopwords.txt
-	min_word_len	= 2
-	min_infix_len = 1
-	enable_star = 1
-	charset_type	= utf-8
+    source        = src_main_%(wikiname)s
+    path          = /var/data/sphinx/main_%(wikiname)s
+    docinfo       = extern
+    morphology    = stem_ru
+    #stopwords    = /var/data/sphinx/stopwords.txt
+    min_word_len  = 2
+    min_infix_len = 1
+    enable_star   = 1
+    charset_type  = utf-8
 }
 
 index inc_%(wikiname)s : main_%(wikiname)s
 {
-	path		= /var/data/sphinx/inc_%(wikiname)s
-	source	= src_incremental_%(wikiname)s
+    path   = /var/data/sphinx/inc_%(wikiname)s
+    source = src_incremental_%(wikiname)s
 }
 
 indexer
 {
-	mem_limit	= 64M
+    mem_limit = 64M
 }
 
 searchd
 {
-	address		= 127.0.0.1
-	port		= %(port)s
-	log		= /var/log/sphinx/%(wikiname)s.log
-	query_log	= /var/log/sphinx/query-%(wikiname)s.log
-	read_timeout	= 5
-	max_children	= 30
-	pid_file	= /var/log/sphinx/searchd-%(wikiname)s.pid
-	max_matches	= 1000
+    address      = 127.0.0.1
+    port         = %(port)s
+    log          = /var/log/sphinx/%(wikiname)s.log
+    query_log    = /var/log/sphinx/query-%(wikiname)s.log
+    read_timeout = 5
+    max_children = 30
+    pid_file     = /var/log/sphinx/searchd-%(wikiname)s.pid
+    max_matches  = 1000
 }
 """ % wiki
-  lf=open(wiki["wikiname"]+".conf","w")
+  lf = open(wiki["wikiname"]+".conf","w")
   lf.write(ls)
   lf.close()
-  
-wikis+=[{
+
+wikis += [{
     "wikiname" : "wiki",
     "sql_user" : "wiki",
     "sql_pass" : "wiki",
-    "sql_db"	 : "wiki13",
+    "sql_db"   : "wiki13",
     "port"     : 3112,
   }]
-  
 
-wikis+=[{
+wikis += [{
     "wikiname" : "smwiki",
     "sql_user" : "smwiki",
     "sql_pass" : "smwiki",
-    "sql_db"	 : "smwiki13",
+    "sql_db"   : "smwiki13",
     "port"     : 3113,
   }]
 
-wikis+=[{
+wikis += [{
     "wikiname" : "rdwiki",
     "sql_user" : "rdwiki",
     "sql_pass" : "rdwiki",
-    "sql_db"	 : "rdwiki13",
+    "sql_db"   : "rdwiki13",
     "port"     : 3114,
   }]
 
-wikis+=[{
+wikis += [{
     "wikiname" : "sbwiki",
     "sql_user" : "sbwiki",
     "sql_pass" : "sbwiki",
-    "sql_db"	 : "sbwiki13",
+    "sql_db"   : "sbwiki13",
     "port"     : 3115,
   }]
 
-wikis+=[{
+wikis += [{
     "wikiname" : "gzwiki",
     "sql_user" : "gzwiki",
     "sql_pass" : "gzwiki",
-    "sql_db"	 : "gzwiki13",
+    "sql_db"   : "gzwiki13",
     "port"     : 3116,
   }]
 
-wikis+=[{
+wikis += [{
     "wikiname" : "dpwiki",
     "sql_user" : "dpwiki",
     "sql_pass" : "dpwiki",
-    "sql_db"	 : "dpwiki13",
+    "sql_db"   : "dpwiki13",
     "port"     : 3117,
   }]
 
-wikis+=[{
+wikis += [{
     "wikiname" : "hrwiki",
     "sql_user" : "hrwiki",
     "sql_pass" : "hrwiki",
-    "sql_db"	 : "hrwiki13",
+    "sql_db"   : "hrwiki13",
     "port"     : 3118,
   }]
 
-wikis+=[{
+wikis += [{
     "wikiname" : "cbwiki",
     "sql_user" : "cbwiki",
     "sql_pass" : "cbwiki",
-    "sql_db"	 : "cbwiki13",
+    "sql_db"   : "cbwiki13",
     "port"     : 3119,
   }]
 
-wikis+=[{
+wikis += [{
     "wikiname" : "gzstable13",
     "sql_user" : "gzstable13",
     "sql_pass" : "gzstable13",
-    "sql_db"	 : "gzstable13",
+    "sql_db"   : "gzstable13",
     "port"     : 3120,
   }]
 
-wikis+=[{
+wikis += [{
     "wikiname" : "orwiki",
     "sql_user" : "orwiki13",
     "sql_pass" : "orwiki13",
-    "sql_db"	 : "orwiki13",
+    "sql_db"   : "orwiki13",
     "port"     : 3121,
   }]
 
-
 for wiki in wikis:
   print_wiki_conf(wiki)
-  
 
-sphinxsearchd=""
+sphinxsearchd = ""
 for wiki in wikis:
-  sphinxsearchd+="""  
+  sphinxsearchd += """
 /usr/local/bin/searchd --config /etc/sphinxsearch/%(wikiname)s.conf &""" % wiki
 
 sphinxsearchd="""#!/bin/sh
@@ -176,11 +172,11 @@ sphinxsearchd="""#!/bin/sh
 exit 0
 """ % sphinxsearchd
 
-lf=open("sphinxsearchd","w")
+lf = open("sphinxsearchd","w")
 lf.write(sphinxsearchd.replace("\r",""))
 lf.close()
-if os.name=="posix":
-  cmd="chmod a+x sphinxsearchd"
+if os.name == "posix":
+  cmd = "chmod a+x sphinxsearchd"
   os.system(cmd)
 
 try:
@@ -194,44 +190,43 @@ except:
   pass
 
 for wiki in wikis:
-  ls="/usr/local/bin/indexer --config /etc/sphinxsearch/%(wikiname)s.conf main_%(wikiname)s --rotate &> /dev/null" % wiki
-  ls="""#!/bin/sh
+  ls = "/usr/local/bin/indexer --config /etc/sphinxsearch/%(wikiname)s.conf main_%(wikiname)s --rotate > /dev/null" % wiki
+  ls = """#!/bin/sh
 
 %s
 """ % ls
-  scriptname="cron.daily/reindex_%(wikiname)s" % wiki
-  lf=open(scriptname,"w")
+  scriptname = "cron.daily/reindex_%(wikiname)s" % wiki
+  lf = open(scriptname,"w")
   lf.write(ls.replace("\r",""))
   lf.close()
-  if os.name=="posix":
-    cmd="chmod a+x "+scriptname
+  if os.name == "posix":
+    cmd = "chmod a+x "+scriptname
     os.system(cmd)
 
-  ls="/usr/local/bin/indexer --config /etc/sphinxsearch/%(wikiname)s.conf inc_%(wikiname)s --rotate &> /dev/null" % wiki
-  ls="""#!/bin/sh
+  ls = "/usr/local/bin/indexer --config /etc/sphinxsearch/%(wikiname)s.conf inc_%(wikiname)s --rotate > /dev/null" % wiki
+  ls = """#!/bin/sh
 
 %s
 """ % ls
-  scriptname="cron.hourly/reindex_%(wikiname)s" % wiki
-  lf=open(scriptname,"w")
+  scriptname = "cron.hourly/reindex_%(wikiname)s" % wiki
+  lf = open(scriptname,"w")
   lf.write(ls.replace("\r",""))
   lf.close()
-  if os.name=="posix":
-    cmd="chmod a+x "+scriptname
+  if os.name == "posix":
+    cmd = "chmod a+x "+scriptname
     os.system(cmd)
-    
-ls=""    
+
+ls = ""
 for wiki in wikis:
-  ls+="\n/usr/local/bin/indexer --config /etc/sphinxsearch/%(wikiname)s.conf main_%(wikiname)s --all " % wiki
-ls="""#!/bin/sh
+  ls += "\n/usr/local/bin/indexer --config /etc/sphinxsearch/%(wikiname)s.conf main_%(wikiname)s --all " % wiki
+ls = """#!/bin/sh
 
 %s
 """ % ls
-scriptname="reindex_all"
-lf=open(scriptname,"w")
+scriptname = "reindex_all"
+lf = open(scriptname,"w")
 lf.write(ls.replace("\r",""))
 lf.close()
-if os.name=="posix":
-  cmd="chmod a+x "+scriptname
+if os.name == "posix":
+  cmd = "chmod a+x "+scriptname
   os.system(cmd)
-
