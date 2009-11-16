a71aa02188e9c6b7ccdafa1e930218d3655b822d
Vitaliy Filippov <vitalif@mail.ru>
2009-11-16 12:58:20 +0000

    Move SVNIntegration into CustisWikiSettings

--- custisinstall/ServerSettings.php
+++ custisinstall/ServerSettings.php
@@ -12,7 +12,6 @@ require_once($IP.'/extensions/Polls/poll.php');
 require_once($IP.'/extensions/mediawikiquizzer/mediawikiquizzer.php');
 require_once($IP.'/extensions/CategoryTemplate/CategoryTemplate.php');
 require_once($IP.'/extensions/Drafts/Drafts.php');
-#require_once($IP.'/extensions/SVNIntegration/SVNIntegration.setup.php');
 require_once($IP.'/extensions/Wikilog/Wikilog.php');
 require_once($IP.'/extensions/DeleteBatch/DeleteBatch.php');
 require_once($IP.'/extensions/FullLocalImage.php');
