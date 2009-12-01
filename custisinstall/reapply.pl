1d482478ad983b9b2cc36748871d5aadade1d50a
Vitaliy Filippov <vitalif@mail.ru>
2009-12-01 15:17:34 +0000

    Bug 41449 svn:eol-style for gnuwin32 patch

---
+++ custisinstall/reapply.pl
@@ -0,0 +1,7 @@
+#!/usr/bin/perl
+
+system("svn revert -R includes extensions/Wikilog languages extensions/AnyWikiDraw extensions/MediaFunctions");
+for my $i (glob "custisinstall/patches/Y-*")
+{
+    system("patch -p0 -t --no-backup-if-mismatch < $i");
+}
