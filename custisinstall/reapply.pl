82d60c139df4a1757f9adaa1ca46bffab337f88e
Vitaliy Filippov <vitalif@mail.ru>
2009-12-22 13:43:58 +0000

    tmp

--- custisinstall/reapply.pl
+++ custisinstall/reapply.pl
@@ -1,6 +1,6 @@
 #!/usr/bin/perl
 
-system("svn revert -R includes extensions/Wikilog languages extensions/AnyWikiDraw extensions/MediaFunctions");
+system("svn revert -R includes extensions/Wikilog languages extensions/AnyWikiDraw extensions/MediaFunctions skins/common");
 for my $i (glob "custisinstall/patches/Y-*")
 {
     system("patch -p0 -t --no-backup-if-mismatch < $i");
