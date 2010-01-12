bea1edc74e86683c18d0dbe9368b3b3e812228af
Vitaliy Filippov <vitalif@mail.ru>
2010-01-12 17:21:34 +0000

    Bug 58894 add to reapply

--- custisinstall/reapply.pl
+++ custisinstall/reapply.pl
@@ -1,6 +1,6 @@
 #!/usr/bin/perl
 
-system("svn revert -R includes extensions/Wikilog languages extensions/AnyWikiDraw extensions/MediaFunctions skins/common");
+system("svn revert -R includes extensions/Wikilog languages extensions/AnyWikiDraw extensions/MediaFunctions extensions/Cite skins/common");
 for my $i (glob "custisinstall/patches/Y-*")
 {
     system("patch -p0 -t --no-backup-if-mismatch < $i");
