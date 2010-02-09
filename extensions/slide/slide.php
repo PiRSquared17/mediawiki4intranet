060c5b2ab51a29c9e6a8278e42fe6051fdbc9dfb
Vitaliy Filippov <vitalif@mail.ru>
2010-02-09 19:53:38 +0000

    Bug 58102 Improvements, modifications, S5 update, 'custis' style

--- extensions/slide/slide.php
+++ extensions/slide/slide.php
@@ -1,6 +1,8 @@
 <?php
-# Copyright (C) 2005 TooooOld <tianshuen@gmail.com>
-# http://www.mediawiki.org/
+
+# Copyright (C) 2010 Vitaliy Filippov <vitalif@mail.ru>
+# Based on (C) 2005 TooooOld <tianshuen@gmail.com>
+# http://meta.wikimedia.org/wiki/User:BR/use_S5_slide_system_in_the_mediawiki/en
 #
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -25,169 +27,52 @@
  * @subpackage Extensions
  */
 
-if( !defined( 'MEDIAWIKI' ) ) {
-	die();
-}
+if (!defined('MEDIAWIKI'))
+    die();
 
-define( 'SLIDE_INC_MARK', 	'(step)' );
-define( 'SLIDE_PAGE_BREAK', '\\\\\\\\' );
-$ce_slide_tpl_file 	= "$IP/extensions/slide/slide.htm";
+/* Default configuration */
 
-$wgExtensionFunctions[] = 'setupSlideShow';
+/* If set to a non-empty value, S5SlideShow will treat as slides
+   only sections with headings matching this value in the end or in the beginning */
+if (!$egS5SlideHeadingMark)
+    $egS5SlideHeadingMark = '';
 
-function setupSlideShow() {
+/* If set to a non-empty value, S5SlideShow will show slides
+   with headings matching this value in the end or in the beginning
+   incrementally */
+if (!$egS5SlideIncMark)
+    $egS5SlideIncMark = '(step)';
 
-	global $wgParser, $wgRequest;
+/* If set to a non-empty value, S5SlideShow will allow to break down slides by this value */
+if (is_null($egS5SlidePageBreak))
+    $egS5SlidePageBreak = "\\\\";
 
-    $wgParser->setHook( 'slide', 'slide' );
+/* Filesystem path to presentation template file */
+if (!$egS5SlideTemplateFile)
+    $egS5SlideTemplateFile = dirname(__FILE__).'/slide.htm';
 
-	$ce_gen			= $wgRequest->getText('ce_gen', false);
-	$ce_slide_title   = $wgRequest->getText('title', false);
+/* Extension setup */
 
-	$ce_slide		= $wgRequest->getText('ce_slide', false);
-	$ce_slide_style	= $wgRequest->getText('ce_style', false);
+$wgExtensionFunctions[] = 'S5SlideShow_Setup';
+$wgHooks['UnknownAction'][] = 'S5SlideShow_UnknownAction';
+$wgAutoloadClasses['S5SlideShow'] = dirname(__FILE__).'/slide.class.php';
 
-	if($ce_slide){
-		$ceTitle =& Title::newFromURL($ce_slide_title);
-		$slideShow = new slideShow($ceTitle, $ce_slide_style);
-		$slideShow->genSlideFile();
-	}
-}
-
-function slide( $style = 'default' ) {
-
- 	global $wgTitle, $wgScriptPath;
-
-	if(is_object($wgTitle)){
-		$slideShow = new slideShow($wgTitle, $style);
-		$url = $wgTitle->escapeLocalURL("ce_style=$style&ce_slide=true");
-		return '<div class="floatright"><span>
-				<a href="'.$url.'" class="image" title="Slide Show" target="_blank">
-				<img src="'.$wgScriptPath.'/extensions/slide/'.$slideShow->style.'/'.$slideShow->style.'.gif" alt="Slide Show" width="240px" /><br />
-				Slide Show</a></span></div>';
-	}else{
-		return "this is a slide show page.\n";
-	}
-}
-
-class slideShow
+function S5SlideShow_Setup()
 {
- 	var $sTitle;
- 	var $style;
- 	var $mContent;
- 	var $mSlides;
- 	var $ts;
-
-	function slideShow($slideTitle, $style = 'default'){
-		
-		if(is_object($slideTitle)){
-		 	$this->sTitle = $slideTitle->getFullText();
-			$slideArticle = new Article( $slideTitle );
-			$this->ts = $slideArticle->getTimestamp();
-			$this->mContent = $slideArticle->getContent(0);
-			$this->setStyle($style);
-			$this->slideParser();
-		}else{
-			wfDebug("Slide: Error! Pass a title object, NOT a title string!\n");
-		}
-	}
-	
-	function setStyle($style = 'default'){
-		$this->style = $style;
-	}
-
-	function slideParser(){
-		$secs = preg_split(
-			'/(^==[^=].*?==)(?!\S)/mi',
-			$this->mContent, -1,
-			PREG_SPLIT_DELIM_CAPTURE);
-
-		$this->mSlides = array();
-
-		$secCount = count($secs);
-		for($i=1; $i<$secCount; $i=$i+2)
-		{
-		 	$this->mSlides[] = array('title' => str_replace('==', '', $secs[$i]),
-		 							  'content' => $secs[$i+1]);
-		}
-		$this->desc = $secs[0];
-		return true;
-	}
-
-	function genSlideFile(){
-
-		global $ce_slide_tpl_file, $ce_file_dir, $ce_slide_tpl,
-				$wgUser, $wgContLang, $wgOut;
-
-	 	if(empty($this->mSlides)){
-	 	 	return false;
-	 	}
-
-		#get template
-		$ce_slide_tpl = @file_get_contents($ce_slide_tpl_file);
-		if( '' == $ce_slide_tpl ){
-			return false;
-		}
-
-		#generate content
-		$fc = '';
-		$s = "<div class=\"slide\"><h1>%s</h1><div class=\"slidecontent\">%s</div></div>\n";
-
-		$options =& ParserOptions::newFromUser( $wgUser );
-		$fileParser = new Parser;
-		
-		$fileParser->setHook( 'slide','ceFakeSlide' );
-		$nt = & Title::newFromText( $this->sTitle );
-
-		foreach( $this->mSlides as $slide ){
-			
-			$title = $slide['title'];
-
-//			if( ! preg_match( '/^'.SLIDE_PAGE_BREAK.'/mi', $slide['content']) ){
-			if( ! preg_match( '/'.SLIDE_PAGE_BREAK.'$/mi', $slide['content']) ){
-//			if( ! strpos( $slide['content'], SLIDE_PAGE_BREAK ) ){
-				$output =& $fileParser->parse($slide['content']."\n__NOTOC__\n__NOEDITSECTION__", $nt, $options);
-				$slideContent = $output->getText();
-				if(strpos($title, SLIDE_INC_MARK)){
-					$slideContent = str_replace('<ul>', '<ul class="incremental">', $slideContent);
-					$slideContent = str_replace('<ol>', '<ol class="incremental">', $slideContent);
-					$title = str_replace(SLIDE_INC_MARK, '', $title);
-				}
-				$fc .= sprintf($s, $title, $slideContent);
-			} else {
-//				$ms = explode( SLIDE_PAGE_BREAK, $slide['content'] );
-				$ms = preg_split( '/'.SLIDE_PAGE_BREAK.'$/mi', $slide['content'] );
-				$sc = count($ms);
-				foreach( $ms as $i=>$ss ){
-					$title = $slide['title'] . " (".($i+1)."/$sc)";
-					$output =& $fileParser->parse($ss."\n__NOTOC__\n__NOEDITSECTION__", $nt, $options);
-					$slideContent = $output->getText();
-					if(strpos($title, SLIDE_INC_MARK)){
-						$slideContent = str_replace('<ul>', '<ul class="incremental">', $slideContent);
-						$slideContent = str_replace('<ol>', '<ol class="incremental">', $slideContent);
-						$title = str_replace(SLIDE_INC_MARK, '', $title);
-					}
-					$fc .= sprintf($s, $title, $slideContent);
-				}
-			} //<--} else {
-		} //<--foreach( $this->mSlides as $slide ){
-		
-		$output =& $fileParser->parse($this->desc."\n__NOTOC__\n__NOEDITSECTION__", $nt, $options);
-		$desc = $output->getText();
-
-		#write to file
-		$ce_page_search = array( '[desc]', '[slideContent]', '[slideTitle]', '[slideStyle]' );
-		$ce_page_replace= array( $desc, $fc, $this->sTitle, $this->style );
-		$fileContent = str_replace($ce_page_search, $ce_page_replace, $ce_slide_tpl);
-		$fileContent = $wgContLang->Convert($fileContent);
-
-		$wgOut->disable();
-		echo($fileContent);
-		exit();
-	}
-
+    global $wgParser;
+    $wgParser->setHook('slide', 'S5SlideShow::slide');
 }
 
-function ceFakeSlide(){}
-
-?>
+function S5SlideShow_UnknownAction($action, $article)
+{
+    if ($action == 'slide')
+    {
+        global $wgRequest;
+        $slideShow = new S5SlideShow($article->getTitle());
+        if ($style = trim($wgRequest->getText('s5style')))
+            $slideShow->style = $style;
+        $slideShow->genSlideFile();
+        return false;
+    }
+    return true;
+}
