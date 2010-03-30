ee0694b5ee4e32620c6fc4d6a7417c60b3ec5497
Vitaliy Filippov <vitali@st-filippov.office.custis.ru>
2010-03-30 22:05:36 +0400

    Bug 62054

--- extensions/CategoryTree/CategoryPageSubclass.php
+++ extensions/CategoryTree/CategoryPageSubclass.php
@@ -195,7 +195,8 @@ class CategoryTreeCategoryViewer extends CategoryViewer {
             while ($i < $n-1 && $start_char[$i] == $start_char[$i+1])
                 $i++;
             /* Group last 1-char subtitle also */
-            if ($e == $s && ($i == $n-2 || mb_strlen($start_char[$i+1]) > 1))
+            if ($e == $s && mb_strlen($start_char[$i+1]) == 1 &&
+                ($i == $n-2 || mb_strlen($start_char[$i+2]) > 1))
                 $e = ++$i;
             if ($e > $s)
             {
