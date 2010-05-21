7779c02d8fbdc720743c6f58d6cf1ace3d144085
Vitaliy Filippov <vitalif@mail.ru>
2010-05-21 16:07:39 +0000

    Bug 64385

--- custisinstall/reapply.pl
+++ custisinstall/reapply.pl
@@ -2,7 +2,7 @@
 
 unlink("extensions/Wikilog/WikilogCalendar.php");
 unlink("extensions/CategoryTree/SubcatCat.i18n.php");
-system("svn revert -R includes extensions/Wikilog extensions/CategoryTree languages extensions/AnyWikiDraw extensions/MediaFunctions extensions/Cite skins/common");
+system("svn revert -R includes extensions/Wikilog extensions/CategoryTree languages extensions/AnyWikiDraw extensions/MediaFunctions extensions/Cite skins/common extensions/DeleteBatch");
 for my $i (glob "custisinstall/patches/Y-*")
 {
     system("patch -p0 -t --no-backup-if-mismatch < $i");
