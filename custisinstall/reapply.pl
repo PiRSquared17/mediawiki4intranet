dbae69760d3651d382ccef004c30958502093709
Vitaliy Filippov <vitalif@mail.ru>
2010-07-06 16:02:35 +0000

    Bug 51595 - Move Wikilog to our SVN (too many patches!)

--- custisinstall/reapply.pl
+++ custisinstall/reapply.pl
@@ -1,9 +1,7 @@
 #!/usr/bin/perl
 
-unlink("extensions/Wikilog/WikilogCalendar.php");
-unlink("extensions/Wikilog/archives/patch-visits.sql");
 unlink("extensions/CategoryTree/SubcatCat.i18n.php");
-system("svn revert -R includes extensions/Wikilog extensions/CategoryTree languages extensions/AnyWikiDraw extensions/MediaFunctions extensions/Cite skins/common extensions/DeleteBatch");
+system("svn revert -R includes extensions/CategoryTree languages extensions/AnyWikiDraw extensions/MediaFunctions extensions/Cite skins/common extensions/DeleteBatch");
 for my $i (glob "custisinstall/patches/Y-*")
 {
     system("patch -p0 -t --no-backup-if-mismatch < $i");
