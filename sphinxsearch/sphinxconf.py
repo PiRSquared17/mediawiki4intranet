2b423c24ebfb2bd2fa79645f6a22a250ffdeb948
Vitaliy Filippov <vitalif@mail.ru>
2010-07-15 10:27:32 +0000

    Bug 58895 - Refactor sphinxconf.py

--- sphinxsearch/sphinxconf.py
+++ sphinxsearch/sphinxconf.py
@@ -13,10 +13,9 @@ import re
 reload(sys)
 sys.setdefaultencoding('utf-8')
 
-wikis = []
+def wiki_conf(wiki):
+  return """### %(wikiname)s ###
 
-def print_wiki_conf(wiki):
-  ls = """
 source src_main_%(wikiname)s
 {
     type           = mysql
@@ -57,177 +56,123 @@ index inc_%(wikiname)s : main_%(wikiname)s
     source = src_incremental_%(wikiname)s
 }
 
-indexer
-{
-    mem_limit = 64M
-}
-
-searchd
-{
-    address      = 127.0.0.1
-    port         = %(port)s
-    log          = /var/log/sphinx/%(wikiname)s.log
-    query_log    = /var/log/sphinx/query-%(wikiname)s.log
-    read_timeout = 5
-    max_children = 30
-    pid_file     = /var/log/sphinx/searchd-%(wikiname)s.pid
-    max_matches  = 1000
-}
 """ % wiki
-  lf = open(wiki["wikiname"]+".conf","w")
-  lf.write(ls)
-  lf.close()
 
-wikis += [{
+wikis_srv = [
+  {
     "wikiname" : "wiki",
     "sql_user" : "wiki",
     "sql_pass" : "wiki",
     "sql_db"   : "wiki13",
-    "port"     : 3112,
-  }]
+  },
+  {
+    "wikiname" : "mmwiki",
+    "sql_user" : "mmwiki",
+    "sql_pass" : "mmwiki",
+    "sql_db"   : "mmwiki",
+  },
+  {
+    "wikiname" : "uml2wiki",
+    "sql_user" : "uml2wiki",
+    "sql_pass" : "uml2wiki",
+    "sql_db"   : "uml2wiki",
+  },
+]
 
-wikis += [{
+wikis_goblin = [
+  {
+    "wikiname" : "wiki",
+    "sql_user" : "wiki",
+    "sql_pass" : "wiki",
+    "sql_db"   : "wiki13",
+  },
+  {
     "wikiname" : "smwiki",
     "sql_user" : "smwiki",
     "sql_pass" : "smwiki",
     "sql_db"   : "smwiki13",
-    "port"     : 3113,
-  }]
-
-wikis += [{
+  },
+  {
     "wikiname" : "rdwiki",
     "sql_user" : "rdwiki",
     "sql_pass" : "rdwiki",
     "sql_db"   : "rdwiki13",
-    "port"     : 3114,
-  }]
-
-wikis += [{
+  },
+  {
     "wikiname" : "sbwiki",
     "sql_user" : "sbwiki",
     "sql_pass" : "sbwiki",
     "sql_db"   : "sbwiki13",
-    "port"     : 3115,
-  }]
-
-wikis += [{
+  },
+  {
     "wikiname" : "gzwiki",
     "sql_user" : "gzwiki",
     "sql_pass" : "gzwiki",
     "sql_db"   : "gzwiki13",
-    "port"     : 3116,
-  }]
-
-wikis += [{
+  },
+  {
     "wikiname" : "dpwiki",
     "sql_user" : "dpwiki",
     "sql_pass" : "dpwiki",
     "sql_db"   : "dpwiki13",
-    "port"     : 3117,
-  }]
-
-wikis += [{
+  },
+  {
     "wikiname" : "hrwiki",
     "sql_user" : "hrwiki",
     "sql_pass" : "hrwiki",
     "sql_db"   : "hrwiki13",
-    "port"     : 3118,
-  }]
-
-wikis += [{
+  },
+  {
     "wikiname" : "cbwiki",
     "sql_user" : "cbwiki",
     "sql_pass" : "cbwiki",
     "sql_db"   : "cbwiki13",
-    "port"     : 3119,
-  }]
-
-wikis += [{
+  },
+  {
     "wikiname" : "gzstable13",
     "sql_user" : "gzstable13",
     "sql_pass" : "gzstable13",
     "sql_db"   : "gzstable13",
-    "port"     : 3120,
-  }]
-
-wikis += [{
+  },
+  {
     "wikiname" : "orwiki",
     "sql_user" : "orwiki13",
     "sql_pass" : "orwiki13",
     "sql_db"   : "orwiki13",
-    "port"     : 3121,
-  }]
+  }
+]
 
+wikis = wikis_goblin
+for i in range(1, len(sys.argv)):
+  if sys.argv[i] == '--srv':
+    wikis = wikis_srv
+  elif sys.argv[i] == '-h' or sys.argv[i] == '--help':
+    print "USAGE: python sphinxconf.py [--srv]"
+    quit()
+
+if os.path.exists("sphinx.conf"):
+  print "sphinx.conf already exists, delete or backup it before reconfiguring"
+  quit()
+
+lf = open("sphinx.conf", "w")
 for wiki in wikis:
-  print_wiki_conf(wiki)
+  lf.write(wiki_conf(wiki))
+lf.write("""### General configuration ###
 
-sphinxsearchd = ""
-for wiki in wikis:
-  sphinxsearchd += """
-/usr/local/bin/searchd --config /etc/sphinxsearch/%(wikiname)s.conf &""" % wiki
+indexer
+{
+    mem_limit = 128M
+}
 
-sphinxsearchd="""#!/bin/sh
-
-%s
-
-exit 0
-""" % sphinxsearchd
-
-lf = open("sphinxsearchd","w")
-lf.write(sphinxsearchd.replace("\r",""))
+searchd
+{
+    listen       = 127.0.0.1:3112
+    log          = /var/log/sphinx/sphinx.log
+    query_log    = /var/log/sphinx/query.log
+    read_timeout = 5
+    max_children = 30
+    pid_file     = /var/run/searchd.pid
+    max_matches  = 1000
+}
+""")
 lf.close()
-if os.name == "posix":
-  cmd = "chmod a+x sphinxsearchd"
-  os.system(cmd)
-
-try:
-  os.mkdir("cron.daily")
-except:
-  pass
-
-try:
-  os.mkdir("cron.hourly")
-except:
-  pass
-
-for wiki in wikis:
-  ls = "/usr/local/bin/indexer --config /etc/sphinxsearch/%(wikiname)s.conf main_%(wikiname)s --rotate > /dev/null" % wiki
-  ls = """#!/bin/sh
-
-%s
-""" % ls
-  scriptname = "cron.daily/reindex_%(wikiname)s" % wiki
-  lf = open(scriptname,"w")
-  lf.write(ls.replace("\r",""))
-  lf.close()
-  if os.name == "posix":
-    cmd = "chmod a+x "+scriptname
-    os.system(cmd)
-
-  ls = "/usr/local/bin/indexer --config /etc/sphinxsearch/%(wikiname)s.conf inc_%(wikiname)s --rotate > /dev/null" % wiki
-  ls = """#!/bin/sh
-
-%s
-""" % ls
-  scriptname = "cron.hourly/reindex_%(wikiname)s" % wiki
-  lf = open(scriptname,"w")
-  lf.write(ls.replace("\r",""))
-  lf.close()
-  if os.name == "posix":
-    cmd = "chmod a+x "+scriptname
-    os.system(cmd)
-
-ls = ""
-for wiki in wikis:
-  ls += "\n/usr/local/bin/indexer --config /etc/sphinxsearch/%(wikiname)s.conf main_%(wikiname)s --all --rotate" % wiki
-ls = """#!/bin/sh
-
-%s
-""" % ls
-scriptname = "reindex_all"
-lf = open(scriptname,"w")
-lf.write(ls.replace("\r",""))
-lf.close()
-if os.name == "posix":
-  cmd = "chmod a+x "+scriptname
-  os.system(cmd)
