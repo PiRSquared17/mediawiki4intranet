e614b6b9ecc9c94f7746d69fab38b2567af665ab
Vitaliy Filippov <vitalif@mail.ru>
2010-07-26 13:55:06 +0000

    Bug 62536 - indenting and svn:eol-style in recaptcha extension

--- extensions/recaptcha/ReCaptcha.i18n.php
+++ extensions/recaptcha/ReCaptcha.i18n.php
@@ -6,7 +6,7 @@
 */
 
 function efReCaptchaMessages() {
-	return array(
+    return array(
 
 /* English */
 'en' => array(
@@ -14,15 +14,14 @@ function efReCaptchaMessages() {
         'recaptcha-addurl' => 'Your edit includes new external links. To help protect against automated
 spam, please type the two words you see in the box below:',
     'recaptcha-badpass' => 'To help protect against automated password cracking, please type the two words you see in the box below:',
-	'recaptcha-createaccount' => 'To help protect against automated account creation, please type the two words you see in the box below:',
-	'recaptcha-createaccount-fail' => "Incorrect or missing reCAPTCHA answer.",
-	'recaptcha-create' => 'To help protect against automated page creation, please type the two words you see in the box below:', 
+    'recaptcha-createaccount' => 'To help protect against automated account creation, please type the two words you see in the box below:',
+    'recaptcha-createaccount-fail' => "Incorrect or missing reCAPTCHA answer.",
+    'recaptcha-create' => 'To help protect against automated page creation, please type the two words you see in the box below:', 
 
 ),
 
 /* TODO - more languages */
 
-	);
+    );
 
 }
-?>
