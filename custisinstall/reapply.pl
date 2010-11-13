e2eb20fc60e54adc1fe73192ee5336e3011805c9
Vitaliy Filippov <vitalif@mail.ru>
2010-11-13 15:52:48 +0000

    Bug 71834

--- custisinstall/reapply.pl
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/perl
-# TODO переписать на PHP
-
-unlink("extensions/CategoryTree/SubcatCat.i18n.php");
-system("svn revert -R includes extensions/CategoryTree languages extensions/AnyWikiDraw extensions/MediaFunctions extensions/Cite skins/common extensions/DeleteBatch extensions/Interwiki");
-for my $i (glob "custisinstall/patches/Y-*")
-{
-    system("patch -p0 -t --no-backup-if-mismatch < $i");
-}
