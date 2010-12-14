b5dcd23e10c45bd7db583471c6f3167b2d2bb281
Vitaliy Filippov <vitali@st-filippov.office.custis.ru>
2010-12-14 18:31:38 +0300

    Bug 57350 - Add patch

--- extensions/PdfHandler/PdfHandler_body.php
+++ extensions/PdfHandler/PdfHandler_body.php
@@ -113,7 +113,7 @@ class PdfHandler extends ImageHandler {
 		if ( !wfMkdirParents( dirname( $dstPath ) ) )
 			return $this->doThumbError( $width, $height, 'thumbnail_dest_directory' );
 
-		$cmd = '(' . $wgPdfProcessor;
+		$cmd = '(' . wfEscapeShellArg( $wgPdfProcessor );
 		$cmd .= " -sDEVICE=jpeg -sOutputFile=- -dFirstPage={$page} -dLastPage={$page}";
 		$cmd .= " -r{$wgPdfHandlerDpi} -dBATCH -dNOPAUSE -q ". wfEscapeShellArg( $srcPath );
 		$cmd .= " | " . wfEscapeShellArg( $wgPdfPostProcessor );
