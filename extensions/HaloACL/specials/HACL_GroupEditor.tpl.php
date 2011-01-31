c407cca6653b4fa69ba1030a414f7e90eb01ba30
Vitaliy Filippov <vitalif@mail.ru>
2011-01-31 19:40:52 +0000

    Bug 74522 - check/uncheck on Enter

--- extensions/HaloACL/specials/HACL_GroupEditor.tpl.php
+++ extensions/HaloACL/specials/HACL_GroupEditor.tpl.php
@@ -1,17 +1,19 @@
-<form action="<?= $wgScript ?>?action=submit" method="POST">
-<input type="hidden" name="wpEditToken" value="<?= htmlspecialchars($wgUser->editToken()) ?>" />
-<input type="hidden" name="wpEdittime" value="<?= $grpTitle ? $grpArticle->getTimestamp() : '' ?>" />
-<input type="hidden" name="wpStarttime" value="<?= wfTimestampNow() ?>" />
-<input type="hidden" id="wpTitle" name="title" value="<?= $grpTitle ? htmlspecialchars($grpTitle->getPrefixedText()) : '' ?>" />
 <table class="acle">
 <tr>
  <td style="vertical-align: top; width: 500px">
-  <p>
-   <b><?= wfMsg('hacl_grp_name') ?></b>
-   <input type="text" id="grp_name" style="width: 200px" onchange="GE.name_change(true)" onkeyup="GE.name_change()"  />
-  </p>
-  <p><b><?= wfMsg('hacl_grp_definition_text') ?></b></p>
-  <p><textarea id="grp_def" name="wpTextbox1" rows="6" style="width: 500px" onchange="GE.parse_fill_indirect()"><?= $grpTitle ? htmlspecialchars($grpArticle->getContent()) : '' ?></textarea></p>
+  <form action="<?= $wgScript ?>?action=submit" method="POST" id="groupEditForm">
+   <input type="hidden" name="wpEditToken" value="<?= htmlspecialchars($wgUser->editToken()) ?>" />
+   <input type="hidden" name="wpEdittime" value="<?= $grpTitle ? $grpArticle->getTimestamp() : '' ?>" />
+   <input type="hidden" name="wpStarttime" value="<?= wfTimestampNow() ?>" />
+   <input type="hidden" id="wpTitle" name="title" value="<?= $grpTitle ? htmlspecialchars($grpTitle->getPrefixedText()) : '' ?>" />
+   <input type="hidden" name="wpSave" value="Save" />
+   <p>
+    <b><?= wfMsg('hacl_grp_name') ?></b>
+    <input type="text" id="grp_name" style="width: 200px" onchange="GE.name_change(true)" onkeyup="GE.name_change()"  />
+   </p>
+   <p><b><?= wfMsg('hacl_grp_definition_text') ?></b></p>
+   <p><textarea id="grp_def" name="wpTextbox1" rows="6" style="width: 500px" onchange="GE.parse_fill_indirect()"><?= $grpTitle ? htmlspecialchars($grpArticle->getContent()) : '' ?></textarea></p>
+  </form>
  </td>
  <td style="vertical-align: top">
   <table>
@@ -43,14 +45,13 @@
 </table>
 <p id="grp_pns">
  <span><a id="grp_pn" class="acl_pn" href="#"></a></span>
- <input type="submit" name="wpSave" value="<?= wfMsg($grpTitle ? 'hacl_grp_save' : 'hacl_grp_create') ?>" id="wpSave" />
+ <input type="button" value="<?= wfMsg($grpTitle ? 'hacl_grp_save' : 'hacl_grp_create') ?>" id="wpSave" onclick="document.getElementById('groupEditForm').submit()" />
  <a id="grp_delete_link" href="<?= $grpTitle ? $grpTitle->getLocalUrl(array('action' => 'delete')) : '' ?>"><?= wfMsg('hacl_grp_delete') ?></a>
 </p>
 <p id="grp_pnhint" class="acl_error" style="display: none"><?= wfMsg('hacl_grp_enter_name_first') ?></p>
 <p id="grp_exists_hint" class="acl_info" style="display: none"><?= wfMsg('hacl_grp_exists') ?></p>
 <p id="grp_define_member" class="acl_error" style="display: none"><?= wfMsg('hacl_grp_define_members') ?></p>
 <p id="grp_define_manager" class="acl_error" style="display: none"><?= wfMsg('hacl_grp_define_managers') ?></p>
-</form>
 
 <script language="JavaScript" src="<?= $haclgHaloScriptPath ?>/scripts/exAttach.js"></script>
 <script language="JavaScript" src="<?= $haclgHaloScriptPath ?>/scripts/offsetRect.js"></script>
