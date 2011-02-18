31ec77c82cbf4261210c4aedd9dfb708de6dc0f0
Vitaliy Filippov <vitalif@mail.ru>
2011-02-18 13:46:39 +0000

    Bug 68935, Bug 60996 - Env proxy for MW 1.16, svn:eol-style native

--- custisinstall/curl-env-proxy.php
+++ custisinstall/curl-env-proxy.php
@@ -4,56 +4,59 @@
 # который, тупая тварь, не умеет это делать самостоятельно. Используется у нас в FeedOnFeeds и MediaWiki.
 # vfilippov@custis.ru, 2010-03-03
 
-function curl_set_env_proxy($curl, $url)
+class CurlEnvProxy
 {
-    if ($proxy = getenv("http_proxy"))
+    static function set($curl, $url)
     {
-        $useproxy = true;
-        if ($url && ($noproxy = preg_split("#\s*,\s*#is", getenv("no_proxy"))))
+        if ($proxy = getenv("http_proxy"))
         {
-            foreach ($noproxy as $n)
+            $useproxy = true;
+            if ($url && ($noproxy = preg_split("#\s*,\s*#is", getenv("no_proxy"))))
             {
-                if (preg_match('#(\d+)\.(\d+)\.(\d+)\.(\d+)/(\d+)#s', $n, $m) &&
-                    preg_match('#^[a-z0-9_]+://(?:[^/]*:[^/]*@)?([^/@]+)(?:/|$|\?)#is', $url, $ip))
+                foreach ($noproxy as $n)
                 {
-                    $mask = array(
-                        max(0x100 - (1 << max( 8-$m[5], 0)), 0),
-                        max(0x100 - (1 << max(16-$m[5], 0)), 0),
-                        max(0x100 - (1 << max(24-$m[5], 0)), 0),
-                        max(0x100 - (1 << max(32-$m[5], 0)), 0),
-                    );
-                    $ip = @gethostbyname($ip[1]);
-                    if (preg_match('#(\d+)\.(\d+)\.(\d+)\.(\d+)#s', $ip, $ipm) &&
-                        (intval($ipm[1]) & $mask[0]) == intval($m[1]) &&
-                        (intval($ipm[2]) & $mask[1]) == intval($m[2]) &&
-                        (intval($ipm[3]) & $mask[2]) == intval($m[3]) &&
-                        (intval($ipm[4]) & $mask[3]) == intval($m[4]))
+                    if (preg_match('#(\d+)\.(\d+)\.(\d+)\.(\d+)/(\d+)#s', $n, $m) &&
+                        preg_match('#^[a-z0-9_]+://(?:[^/]*:[^/]*@)?([^/@]+)(?:/|$|\?)#is', $url, $ip))
                     {
-                        $useproxy = false;
-                        break;
+                        $mask = array(
+                            max(0x100 - (1 << max( 8-$m[5], 0)), 0),
+                            max(0x100 - (1 << max(16-$m[5], 0)), 0),
+                            max(0x100 - (1 << max(24-$m[5], 0)), 0),
+                            max(0x100 - (1 << max(32-$m[5], 0)), 0),
+                        );
+                        $ip = @gethostbyname($ip[1]);
+                        if (preg_match('#(\d+)\.(\d+)\.(\d+)\.(\d+)#s', $ip, $ipm) &&
+                            (intval($ipm[1]) & $mask[0]) == intval($m[1]) &&
+                            (intval($ipm[2]) & $mask[1]) == intval($m[2]) &&
+                            (intval($ipm[3]) & $mask[2]) == intval($m[3]) &&
+                            (intval($ipm[4]) & $mask[3]) == intval($m[4]))
+                        {
+                            $useproxy = false;
+                            break;
+                        }
                     }
-                }
-                else
-                {
-                    $n = preg_replace('/#.*$/is', '', $n);
-                    $n = preg_quote($n);
-                    $n = str_replace('\\*', '.*', $n);
-                    if (preg_match('#'.$n.'#is', $url))
+                    else
                     {
-                        $useproxy = false;
-                        break;
+                        $n = preg_replace('/#.*$/is', '', $n);
+                        $n = preg_quote($n);
+                        $n = str_replace('\\*', '.*', $n);
+                        if (preg_match('#'.$n.'#is', $url))
+                        {
+                            $useproxy = false;
+                            break;
+                        }
                     }
                 }
             }
+            if ($useproxy)
+            {
+                $proxy = preg_replace('#^http://#is', '', $proxy);
+                $proxy = preg_replace('#/*$#is', '', $proxy);
+            }
+            else
+                $proxy = '';
+            curl_setopt($curl, CURLOPT_PROXY, $proxy);
         }
-        if ($useproxy)
-        {
-            $proxy = preg_replace('#^http://#is', '', $proxy);
-            $proxy = preg_replace('#/*$#is', '', $proxy);
-        }
-        else
-            $proxy = '';
-        curl_setopt($curl, CURLOPT_PROXY, $proxy);
+        return $proxy;
     }
-    return $proxy;
 }
