a3f112ebab11b47b3d8a1e2ab564735a3b81548f
Vitaliy Filippov <vitalif@mail.ru>
2011-02-24 15:33:47 +0000

    Bug 74522 - svn:keywords

--- extensions/HaloACL/specials/HACL_ACLSpecial.php
+++ extensions/HaloACL/specials/HACL_ACLSpecial.php
@@ -2,18 +2,19 @@
 
 /* Copyright 2010+, Vitaliy Filippov <vitalif[d.o.g]mail.ru>
  *                  Stas Fomin <stas.fomin[d.o.g]yandex.ru>
- * This file is part of heavily modified "Web 1.0" HaloACL-extension.
- * http://wiki.4intra.net/Mediawiki4Intranet
- * $Id: $
+ * This file is part of IntraACL MediaWiki extension. License: GPLv3.
+ * http://wiki.4intra.net/IntraACL
+ * $Id$
  *
+ * Based on HaloACL
  * Copyright 2009, ontoprise GmbH
  *
- * The HaloACL-Extension is free software; you can redistribute it and/or modify
+ * The IntraACL-Extension is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation; either version 3 of the License, or
  * (at your option) any later version.
  *
- * The HaloACL-Extension is distributed in the hope that it will be useful,
+ * The IntraACL-Extension is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
--- extensions/HaloACL/storage/HACL_DBHelper.php
+++ extensions/HaloACL/storage/HACL_DBHelper.php
@@ -1,24 +1,27 @@
 <?php
-/*  Copyright 2009, ontoprise GmbH
-*   This file is part of the HaloACL-Extension.
-*
-*   The HaloACL-Extension is free software; you can redistribute it and/or modify
-*   it under the terms of the GNU General Public License as published by
-*   the Free Software Foundation; either version 3 of the License, or
-*   (at your option) any later version.
-*
-*   The HaloACL-Extension is distributed in the hope that it will be useful,
-*   but WITHOUT ANY WARRANTY; without even the implied warranty of
-*   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-*   GNU General Public License for more details.
-*
-*   You should have received a copy of the GNU General Public License
-*   along with this program.  If not, see <http://www.gnu.org/licenses/>.
-* 
-*   Created on 19.10.2007
-*
-*   Author: kai
-*/
+
+/* Copyright 2010+, Vitaliy Filippov <vitalif[d.o.g]mail.ru>
+ *                  Stas Fomin <stas.fomin[d.o.g]yandex.ru>
+ * This file is part of IntraACL MediaWiki extension. License: GPLv3.
+ * http://wiki.4intra.net/IntraACL
+ * $Id$
+ *
+ * Based on HaloACL
+ * Copyright 2009, ontoprise GmbH
+ *
+ * The IntraACL-Extension is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * The IntraACL-Extension is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
 
 class HACLDBHelper
 {
