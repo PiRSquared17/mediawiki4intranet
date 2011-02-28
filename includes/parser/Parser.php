a4cd0eed1c120860ed4ed3f659d14abe92151a15
Vitaliy Filippov <vitali@st-filippov.office.custis.ru>
2011-02-28 15:26:49 +0300

    Bug 67060 - Debug

--- includes/parser/Parser.php
+++ includes/parser/Parser.php
@@ -2168,6 +2168,7 @@ class Parser
 				if ( $emptyLines > 1 )
 				{
 					// start a new paragraph
+					$lastParagraphPos = strlen( $output );
 					$this->mLastSection = 'p';
 					$output .= '<p><br />';
 					$emptyLines = 0;
