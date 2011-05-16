40ad7892f07d517800cf5352eef163a819ad15cb
Vitaliy Filippov <vitalif@yourcmc.ru>
2011-05-16 14:56:08 +0400

    merge of .all

--- includes/GlobalFunctions.php
+++ includes/GlobalFunctions.php
@@ -1508,7 +1508,7 @@ function wfMerge( $old, $mine, $yours, &$result ){
 	# case of broken installations.
 	if( !$wgDiff3 || !file_exists( $wgDiff3 ) ) {
 		wfDebug( "diff3 not found\n" );
-		return false;
+		return NULL;
 	}
 
 	# Make temporary files
@@ -1516,6 +1516,10 @@ function wfMerge( $old, $mine, $yours, &$result ){
 	$oldtextFile = fopen( $oldtextName = tempnam( $td, 'merge-old-' ), 'w' );
 	$mytextFile = fopen( $mytextName = tempnam( $td, 'merge-mine-' ), 'w' );
 	$yourtextFile = fopen( $yourtextName = tempnam( $td, 'merge-your-' ), 'w' );
+
+	if ($old{-1} != "\n") $old .= "\n";
+	if ($mine{-1} != "\n") $mine .= "\n";
+	if ($yours{-1} != "\n") $yours .= "\n";
 
 	fwrite( $oldtextFile, $old ); fclose( $oldtextFile );
 	fwrite( $mytextFile, $mine ); fclose( $mytextFile );
@@ -1536,8 +1540,10 @@ function wfMerge( $old, $mine, $yours, &$result ){
 	pclose( $handle );
 
 	# Merge differences
-	$cmd = $wgDiff3 . ' -a -e --merge ' .
-	  wfEscapeShellArg( $mytextName, $oldtextName, $yourtextName );
+	$cmd = $wgDiff3 . ' -a -A --merge ' . wfEscapeShellArg(
+	  '-L', wfMsg( 'merge-mine' ), '-L', wfMsg( 'merge-old' ),
+	  '-L', wfMsg( 'merge-their' ), $mytextName, $oldtextName, $yourtextName
+	);
 	$handle = popen( $cmd, 'r' );
 	$result = '';
 	do {
