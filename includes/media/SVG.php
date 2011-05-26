1303d84b8b65e4e990f2cf55af961c55d8c7da7b
Vitaliy Filippov <vitalif@yourcmc.ru>
2011-05-26 20:32:47 +0400

    merge of .all

--- includes/media/SVG.php
+++ includes/media/SVG.php
@@ -6,12 +6,13 @@
 
 class SvgThumbnailImage extends ThumbnailImage
 {
-	function SvgThumbnailImage($file, $url, $svgurl, $width, $height, $path = false, $page = false)
+	function SvgThumbnailImage( $file, $url, $svgurl, $width, $height, $path = false, $page = false, $later = false )
 	{
 		$this->svgurl = $svgurl;
-		$this->ThumbnailImage($file, $url, $width, $height, $path, $page);
+		$this->later = $later;
+		$this->ThumbnailImage( $file, $url, $width, $height, $path, $page );
 	}
-	function toHtml($options = array())
+	function toHtml( $options = array() )
 	{
 		if ( count( func_get_args() ) == 2 ) {
 			throw new MWException( __METHOD__ .' called in the old style' );
@@ -51,22 +52,40 @@ class SvgThumbnailImage extends ThumbnailImage
 		if ( !empty( $options['img-class'] ) ) {
 			$attribs['class'] = $options['img-class'];
 		}
-		$attribs['id'] = 'rsvg'.rand();
-		$vecatt = $attribs;
-		$vecatt['id'] = substr( $attribs['id'], 1 );
-		$vecatt['src'] = $this->svgurl;
-		$vecatt['style'] = "display: none;".$vecatt['style'];
-		$html = $this->linkWrap( $linkAttribs,
-			Xml::element( 'img', $attribs ) .
-			Xml::element( 'img', $vecatt )
-		);
-		$html .= 
-'<script type="text/javascript">
-if ( document.implementation.hasFeature( "http"+"://www.w3.org/TR/SVG11/feature#BasicStructure", "1.1" ) ) {
-document.getElementById("'.$attribs['id'].'").style.display = "none";
-document.getElementById("'.$vecatt['id'].'").style.display = "";
-}
-</script>';
+
+		// :-( The only cross-browser way to link from SVG
+		// is to add an <a xlink:href> into SVG image itself
+		global $wgServer;
+		$href = $linkAttribs['href'];
+		if ( $href{0} == '/' )
+			$href = $wgServer . $href;
+		$method = method_exists( $this->file, 'getPhys' ) ? 'getPhys' : 'getName';
+		$hash = '/' . $this->file->$method() . '-linked-' . crc32( $href . "\0" . $linkAttribs['title'] ) . '.svg';
+		$linkfn = $this->file->getThumbPath() . $hash;
+		$linkurl = $this->file->getThumbUrl() . $hash;
+		// Cache changed SVGs only when TRANSFORM_LATER is on
+		if ( $this->later )
+			$mtime = filemtime( $linkfn );
+		if ( !$mtime || $mtime < filemtime( $this->file->getPath() ) )
+		{
+			$svg = file_get_contents( $this->file->getPath() );
+			preg_match( '/<svg[^<>]*>/', $svg, $m, PREG_OFFSET_CAPTURE );
+			$open = $m[0][0];
+			if ( !strpos( $open, 'xmlns:xlink' ) )
+				$open = substr( $open, 0, -1 ) . ' xmlns:xlink="http://www.w3.org/1999/xlink">';
+			$open .= '<a xlink:href="'.htmlspecialchars( $href ).
+				'" target="_parent" xlink:title="'.htmlspecialchars( $linkAttribs['title'] ).'">';
+			$svg = substr( $svg, 0, $m[0][1] ) . $open . substr( $svg, $m[0][1] + strlen( $m[0][0] ) );
+			$svg = str_replace( '</svg>', '</a></svg>', $svg );
+			file_put_contents( $linkfn, $svg );
+		}
+
+		$html = $this->linkWrap( $linkAttribs, Xml::element( 'img', $attribs ) );
+		$html = Xml::tags( 'object', array(
+			'type' => 'image/svg+xml',
+			'data' => $linkurl,
+			'style' => 'overflow: hidden',
+		), $html );
 		return $html;
 	}
 }
@@ -117,7 +136,7 @@ class SvgHandler extends ImageHandler {
 		$srcPath = $image->getPath();
 
 		if ( $flags & self::TRANSFORM_LATER ) {
-			return new SvgThumbnailImage( $image, $dstUrl, $image->getFullUrl(), $clientWidth, $clientHeight, $dstPath );
+			return new SvgThumbnailImage( $image, $dstUrl, $image->getFullUrl(), $clientWidth, $clientHeight, $dstPath, false, true );
 		}
 
 		if ( !wfMkdirParents( dirname( $dstPath ) ) ) {
