35b3bd709d96b371de228478783f6e051090ba46
Vitaliy Filippov <vitalif@mail.ru>
2011-06-07 23:32:29 +0000

    Bug 79838 - AJAX subpage tree

---
+++ extensions/SubPageList2/SubPageList2.i18n.php
@@ -0,0 +1,11 @@
+<?php
+
+$messages = array();
+
+$messages['en'] = array(
+    'subpagelist-view' => 'Subpages ($1) &rarr;',
+);
+
+$messages['ru'] = array(
+    'subpagelist-view' => 'Подстраницы ($1) &rarr;',
+);
