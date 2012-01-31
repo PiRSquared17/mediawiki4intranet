fae01006f736af7c796798902420c026dd20a88f
Vitaliy Filippov <vitalif@yourcmc.ru>
2012-01-31 20:08:32 +0400

    merge of dot-after-tocnumber

--- includes/DefaultSettings.php
+++ includes/DefaultSettings.php
@@ -2865,6 +2865,11 @@ $wgUrlProtocols = array(
 );
 
 /**
+ * If true, add '.' TOC numbers have "x.x.x." format instead of just "x.x.x"
+ */
+$wgDotAfterTocnumber = false;
+
+/**
  * If true, removes (substitutes) templates in "~~~~" signatures.
  */
 $wgCleanSignatures = true;
