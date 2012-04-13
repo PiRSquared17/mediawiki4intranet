88e42a666e1fe20e131ffcf425b9f734b3a60fc5
Vitaliy Filippov <vitalif@yourcmc.ru>
2012-04-13 16:26:48 +0400

    merge of .all

--- extensions/OpenID/SpecialOpenIDLogin.body.php
+++ extensions/OpenID/SpecialOpenIDLogin.body.php
@@ -264,7 +264,7 @@ class SpecialOpenIDLogin extends SpecialOpenID {
 				$fullname = $sreg['fullname'];
 			}
 
-			if ( array_key_exists( 'http://axschema.org/namePerson/first', $ax ) || array_key_exists( 'http://axschema.org/namePerson/last', $ax ) ) {
+			if ( $ax && ( array_key_exists( 'http://axschema.org/namePerson/first', $ax ) || array_key_exists( 'http://axschema.org/namePerson/last', $ax ) ) ) {
 				$fullname = $ax['http://axschema.org/namePerson/first'][0] . " " . $ax['http://axschema.org/namePerson/last'][0];
 			}
 
