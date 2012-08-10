00545e487c7592e2194974858a404864c04b19f2
Vitaliy Filippov <vitalif@yourcmc.ru>
2012-08-10 17:09:01 +0400

    merge of add-hooks

--- extensions/Cite/Cite_body.php
+++ extensions/Cite/Cite_body.php
@@ -1051,12 +1051,12 @@ class Cite {
 	 *
 	 * @return bool
 	 */
-	function checkRefsNoReferences( &$parser, &$text ) {
+	function checkRefsNoReferences( &$parser, &$text, $clearState ) {
 		if ( $parser->extCite !== $this ) {
-			return $parser->extCite->checkRefsNoReferences( $parser, $text );
+			return $parser->extCite->checkRefsNoReferences( $parser, $text, $clearState );
 		}
 		
-		if ( $parser->getOptions()->getIsSectionPreview() ) {
+		if ( $parser->getOptions()->getIsSectionPreview() || !$clearState ) {
 			return true;
 		}
 
@@ -1064,10 +1064,10 @@ class Cite {
 			if ( count( $refs ) == 0 ) {
 				continue;
 			}
-			$text .= "\n<br />";
 			if ( $group == CITE_DEFAULT_GROUP ) {
-				$text .= $this->error( 'cite_error_refs_without_references' );
+				$text .= "<hr />\n" . $this->references( NULL, array(), $parser );
 			} else {
+				$text .= "\n<br />";
 				$text .= $this->error( 'cite_error_group_refs_without_references', htmlspecialchars( $group ) );
 			}
 		}
