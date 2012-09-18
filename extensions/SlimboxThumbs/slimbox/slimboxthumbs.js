d599f467d9a63446a3177afccef9d50df0b7c7ec
Vitaliy Filippov <vitalif@mail.ru>
2012-09-18 11:24:25 +0000

    Bug 108570 - O_o do not save DOM nodes - sometimes they change

--- extensions/SlimboxThumbs/slimbox/slimboxthumbs.js
+++ extensions/SlimboxThumbs/slimbox/slimboxthumbs.js
@@ -9,12 +9,10 @@ function makeSlimboxThumbs( $, pathRegexp, wgFullScriptPath ) {
 	var canview = /\.(jpe?g|jpe|gif|png)$/i;
 	var m;
 	var names = [];
-	var links = {};
 	$( 'img' ).each( function( i, e ) {
 		if ( e.parentNode.nodeName == 'A' && ( m = re.exec( e.parentNode.href ) ) ) {
 			var n = unescape( m[1] );
 			names.push( n );
-			links[ n ] = e.parentNode;
 		}
 	} );
 	if ( names.length ) {
@@ -25,23 +23,29 @@ function makeSlimboxThumbs( $, pathRegexp, wgFullScriptPath ) {
 			var ww = $( window ).width();
 			var wh = $( window ).height() * 0.9;
 			r = $.parseJSON( r.responseText );
-			for ( var n in r ) {
-				var h = r[n][2];
-				can = canview.exec( n );
-				if ( !can || r[n][0] > ww || r[n][1] > wh ) {
-					var sc = ww;
-					var sh = Math.round( r[n][0] * wh / r[n][1] );
-					if ( sh < sc ) {
-						sc = sh;
+			$( 'img' ).each( function( i, e ) {
+				if ( e.parentNode.nodeName == 'A' && ( m = re.exec( e.parentNode.href ) ) ) {
+					var n = unescape( m[1] );
+					if ( !r[n] ) {
+						return;
 					}
-					h = wgFullScriptPath + '/thumb.php?f=' + escape( n ) + '&w=' + sc;
+					var h = r[n][2];
+					can = canview.exec( n );
+					if ( !can || r[n][0] > ww || r[n][1] > wh ) {
+						var sc = ww;
+						var sh = Math.round( r[n][0] * wh / r[n][1] );
+						if ( sh < sc ) {
+							sc = sh;
+						}
+						h = wgFullScriptPath + '/thumb.php?f=' + escape( n ) + '&w=' + sc;
+					}
+					e.parentNode._lightbox = [
+						h, '<a href="'+e.parentNode.href+'">'+
+						n.replace( /_/g, ' ' )+'</a>'
+					];
+					nodes.push( e.parentNode );
 				}
-				links[n]._lightbox = [
-					h, '<a href="'+links[n].href+'">'+
-					n.replace( /_/g, ' ' )+'</a>'
-				];
-				nodes.push( links[n] );
-			}
+			} );
 			$( nodes ).slimbox({ captionAnimationDuration: 0 }, function( e, i ) {
 				return e._lightbox;
 			}, function() { return true; });
